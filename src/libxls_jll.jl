49429b91bf5e36b47c79f77fd8f643cdbf627cb3
jlbuild <juliabuildbot@gmail.com>
2026-08-29 10:42:11 +0000

    libxls_jll build 1.6.3+0

--- src/libxls_jll.jl
+++ src/libxls_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libxls")
-JLLWrappers.@generate_main_file("libxls", UUID("86717fa1-76ed-5f2d-aba0-009d7207923f"))
+JLLWrappers.@generate_main_file("libxls", Base.UUID("86717fa1-76ed-5f2d-aba0-009d7207923f"))
 end  # module libxls_jll
